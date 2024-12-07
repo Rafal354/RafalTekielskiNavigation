634391460d7cd207b112e22f72dcc643126d6df7
Rafal Tekielski <rafaltekielski@gmail.com>
2024-12-07 15:44:17 +0100

    Navigation lab finished

--- app/src/main/java/com/example/rafaltekielskinavigation/MainActivity.kt
+++ app/src/main/java/com/example/rafaltekielskinavigation/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.rafaltekielskinavigation
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.rafaltekielskinavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,23 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        setNavToSecBtnOnClickListener()
+    }
 
-        setNavToSecBtnOnClickListener()
-
-
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
+//    private fun setNavToSecBtnOnClickListener() {
+//        binding.navToSecActivityBtn.setOnClickListener {
+//            val intent = Intent(this, SecondActivity::class.java)
+//            startActivity(intent)
 //        }
-    }
-
-    private fun setNavToSecBtnOnClickListener() {
-        binding.navToSecActivityBtn.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
-        }
-    }
+//    }
 }
--- app/src/main/java/com/example/rafaltekielskinavigation/SecondActivity.kt
+++ app/src/main/java/com/example/rafaltekielskinavigation/SecondActivity.kt
@@ -1,12 +1,15 @@
 package com.example.rafaltekielskinavigation
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,12 @@ class SecondActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backToFragmentButton = findViewById<Button>(R.id.btn_back_to_first_fragment)
+
+        backToFragmentButton.setOnClickListener {
+            finish()
         }
     }
 }
